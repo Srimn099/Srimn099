db913aabdd10484b8123b43d1db744bf3b112df2
Srimn099 <128180950+Srimn099@users.noreply.github.com>
2024-01-08 01:01:16 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+/*!
+* Start Bootstrap - Simple Sidebar v6.0.5 (https://startbootstrap.com/template/simple-sidebar)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
+*/
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+});
+
+function previewImage() {
+    const image = document.querySelector('#image');
+    const imgPreview = document.querySelector('.img-preview');
+
+    imgPreview.style.display = 'block';
+    // imgPreview.style.width = '200px';
+    // imgPreview.style.height = '110px';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+    oFReader.onload = function(oFREvent) {
+        imgPreview.src = oFREvent.target.result;
+    }
+
+};
+
+// Post's Category
+const title = document.querySelector('#title');
+const slug = document.querySelector('#slug');
+
+title.addEventListener('change', function() {
+    fetch('/dashboard/posts/createSlug?title=' + title.value)
+        .then(response => response.json())
+        .then(data => slug.value = data.slug)
+});
